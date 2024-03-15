1e592d6a2ff57ed07c99df468d79f44ab71bcd65
Will Morris <wdm9727@utulsa.edu>
2024-03-14 20:25:45 -0500

    edited problem 1 code

--- Problem 1/exceptionHandling.py
+++ Problem 1/exceptionHandling.py
@@ -1,21 +1,14 @@
 def exceptionHandling():
-    # Split input into 2 parts: name and age
-    parts = input().split()
-    name = parts[0]
+    name = input().split()[0]
     while name != '-1':
-        try: 
+        try:
             parts = input().split()
             name = parts[0]
-            age = int(parts[1])
-            age += 1
+            age = int(parts[1]) + 1
             print(f'{name} {age}')
         except ValueError:
-            print(f'Invalid age for {name}')
+            print(f'{name} 0')
             
-        
-        # Get next line
-        parts = input().split()
-        name = parts[0]
 
 if __name__ == '__main__':
     exceptionHandling()
